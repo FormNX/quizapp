52b5b1b2c1af12da1545522fbd6b1c7610057923
nikhil <nikhila@thewebfosters.com>
2024-02-21 21:12:38 +0530

    Right choice link added

--- pages/index.vue
+++ pages/index.vue
@@ -15,44 +15,49 @@
 
 
         <div class="flex">
-    <!-- Left side with quiz content -->
-    <div class="flex-none w-full md:w-10/12" >
-        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 p-4">
-            <NuxtLink v-for="(quiz, key) in quizzes" :key="key" :to="'/play-quiz?quiz=' + key"
-                class="relative overflow-hidden rounded-lg shadow-md group hover:shadow-lg hover:shadow-emerald-500/50 transition duration-300">
-                <div class="flex-row items-center justify-center h-full p-4 bg-white rounded-lg bg-opacity-90">
-                    <h3 class="text-lg font-semibold mb-2">{{ quiz.label }}</h3>
-                    <p class="text-sm text-gray-600">{{ quiz.description }}</p>
+            <!-- Left side with quiz content -->
+            <div class="flex-none w-full md:w-10/12">
+                <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 p-4">
+                    <NuxtLink v-for="(quiz, key) in quizzes" :key="key" :to="'/play-quiz?quiz=' + key"
+                        class="relative overflow-hidden rounded-lg shadow-md group hover:shadow-lg hover:shadow-emerald-500/50 transition duration-300">
+                        <div class="flex-row items-center justify-center h-full p-4 bg-white rounded-lg bg-opacity-90">
+                            <h3 class="text-lg font-semibold mb-2">{{ quiz.label }}</h3>
+                            <p class="text-sm text-gray-600">{{ quiz.description }}</p>
+                        </div>
+                    </NuxtLink>
                 </div>
-            </NuxtLink>
+            </div>
+
+            <!-- Right side with "Powered by FormNX" ad -->
+            <div class="flex-none w-2/12 p-4">
+                <div class="bg-gray-200 p-4 rounded-lg">
+                    <h3 class="text-lg font-semibold mb-2">Powered by FormNX</h3>
+                    <ul class="list-disc ml-3 my-4">
+                        <li class="text-sm text-gray-600 my-2">Create any type of forms in minutes</li>
+                        <li class="text-sm text-gray-600 my-2">1000's of Templates</li>
+                        <li class="text-sm text-gray-600 my-2">Use free forever</li>
+                    </ul>
+
+                    <a class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800 mt-4"
+                        href="https://formnx.com/?ref=quiz" target="_blank">
+                        <span
+                            class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                            Get Started Free
+                        </span>
+                    </a>
+
+                    
+<a href="https://formnx.com/f/is-formnx-the-right-choice-for-you--17gm21" target="_blank" class="inline-flex items-center justify-center p-5 text-base font-medium text-gray-500 rounded-lg bg-gray-50 hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700 dark:hover:text-white mt-3">                                   
+    <span class="w-full">Is FormNX Right Choice?</span>
+    <svg class="w-4 h-4 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+    </svg>
+</a> 
+
+
+                </div>
+            </div>
         </div>
-    </div>
-
-    <!-- Right side with "Powered by FormNX" ad -->
-    <div class="flex-none w-2/12 p-4">
-        <div class="bg-gray-200 p-4 rounded-lg">
-            <h3 class="text-lg font-semibold mb-2">Powered by FormNX</h3>
-            <ul class="list-disc ml-3 my-4">
-                <li class="text-sm text-gray-600 my-2">Create any type of forms in minutes</li>
-                <li class="text-sm text-gray-600 my-2">1000's of Templates</li>
-                <li class="text-sm text-gray-600 my-2">Use free forever</li>
-            </ul>
-
-            <a class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800 mt-4" href="https://formnx.com/?ref=quiz" target="_blank">
-<span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-    Get Started Free
-</span>
-</a>
-
-
-        </div>
-    </div>
-</div>
-
-
-
-        
-
 
     </div>
 </template>
